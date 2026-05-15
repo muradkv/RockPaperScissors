f0cf73d1b12e7129d8713bfe54bc9f739561457a
murad <muradkv@gmail.com>
2026-05-15 22:28:15 +0300

    Initial commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by murad on 15.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
